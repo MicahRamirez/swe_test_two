62d6c61d43982d330da569accb265cc41bb973a2
MicahRamirez <xaviermicahramirez@utexas.edu>
2016-04-10 17:34:20 -0500

    First questions

---
+++ sets.sql
@@ -0,0 +1,7 @@
+select "*** set union: names of students OR colleges ***";
+
+
+select "*** set intersection: names of students AND colleges ***";
+
+
+select "*** set difference: ID of students who did not apply anywhere ***";
---
+++ subqueries.sql
@@ -0,0 +1,13 @@
+select "*** ID, name, and GPA of students who applied in CS ***";
+
+
+select "*** GPA of students who applied in CS ***";
+
+
+select "*** ID of students who have applied in CS but not in EE ***";
+
+select "*** colleges with another college in the same state ***";
+
+select "*** colleges with highest enrollment ***";
+
+select "*** students with highest GPA ***";
